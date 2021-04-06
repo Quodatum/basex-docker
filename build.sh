d82c87c00c8b86f039294d2a9c99868f2ba88786
apb <bunce.andy@gmail.com>
2021-04-06 23:10:46 +0100

    [fix] manifest

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
+# @see https://www.docker.com/blog/multi-arch-build-and-images-the-simple-way/
+
 image=quodatum/basexhttp
 
 manifest(){
- docker manifest create  "$image" --amend "$image:manifest-amd64" --amend "$image:manifest-armv7" --amend "$image:manifest-arm64v8" 
+ docker manifest create  "$image" --amend "$image:manifest-amd64" --amend "$image:manifest-armv7" --amend "$image:manifest-arm64"
+ docker manifest push "$image" 
 }
+
 make2(){
 # $1 platform, $2 tag eg `make2 linux/amd64 amd64`
  echo "###### docker buildx build --platform $1 --tag $image:manifest-$2 ."
  docker buildx build --platform "$1" --tag "$image:manifest-$2" --push .
 }
 
-#docker manifest push your-username/multiarch-example:manifest-latest
+
 echo hello pushing "$image" to docker hub
 make2 linux/arm/v7 armv7
 make2 linux/arm64 arm64
